a6173afea0a1a1db1dcaec9f2cfac6c766d07ee5
LebedALIV2601 <lebedaliv2601@mail.ru>
2022-10-24 20:39:32 +0300

    beginning to add auth

---
+++ app/src/main/java/com/disgust/sereda/auth/phone/PhoneEnterScreen.kt
@@ -0,0 +1,47 @@
+package com.disgust.sereda.auth.phone
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.navigation.NavHostController
+
+@Composable
+fun PhoneEnterScreen(
+    navController: NavHostController
+) {
+
+    val inputText = remember { mutableStateOf("") }
+    val focusManager = LocalFocusManager.current
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        TextField(
+            value = inputText.value,
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(onDone = {
+                focusManager.clearFocus()
+            }),
+            onValueChange = { inputText.value = it }
+        )
+        Button(onClick = {}) {
+            Text(text = "Get code")
+        }
+    }
+}
---
+++ app/src/main/java/com/disgust/sereda/auth/phone/PhoneEnterViewModel.kt
@@ -0,0 +1,13 @@
+package com.disgust.sereda.auth.phone
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class PhoneEnterViewModel : ViewModel() {
+
+    fun getCode(phone: String) {
+
+    }
+
+}
