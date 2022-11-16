5582192dc76059a7b3caace961066af65b5ec8eb
Alexandr <sanchello2001@yandex.ru>
2022-11-16 23:08:26 +0300

    Загрузка изображений в общем модуле

--- app/src/main/java/com/disgust/sereda/recipe/screens/info/RecipeInfoScreen.kt
+++ app/src/main/java/com/disgust/sereda/recipe/screens/info/RecipeInfoScreen.kt
@@ -1,13 +1,19 @@
 package com.disgust.sereda.recipe.screens.info
 
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.disgust.sereda.recipe.screens.info.interaction.RecipeInfoState
 import com.disgust.sereda.recipe.screens.info.interaction.RecipeInfoUIEvent
 import com.disgust.sereda.utils.DoOnInit
+import com.disgust.sereda.utils.commonViews.ImageIngredientView
+import com.disgust.sereda.utils.commonViews.ImageRecipeView
 
 @Composable
 fun RecipeInfoScreen(
@@ -21,12 +27,58 @@ fun RecipeInfoScreen(
         vm.onUIEvent(RecipeInfoUIEvent.StartScreen(recipeId))
     }
 
-    Text(
-        text =
-        when (recipeInfoState.value) {
-            is RecipeInfoState.Loading -> "Loading $recipeId"
-            is RecipeInfoState.Success -> (recipeInfoState.value as RecipeInfoState.Success).data.toString()
-            is RecipeInfoState.Error -> (recipeInfoState.value as RecipeInfoState.Error).error.toString()
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column() {
+            Box(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .height(300.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                if (recipeInfoState.value is RecipeInfoState.Success)
+                    (recipeInfoState.value as RecipeInfoState.Success).data.image?.let {
+                        ImageRecipeView(
+                            url = it
+                        )
+                    }
+            }
+
+            Box(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .height(300.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                if (recipeInfoState.value is RecipeInfoState.Success)
+                    (recipeInfoState.value as RecipeInfoState.Success).data.ingredients?.get(0)?.imageName?.let {
+                        ImageIngredientView(
+                            url = it
+                        )
+                    }
+            }
+
+            Box(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .height(300.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text =
+                    when (recipeInfoState.value) {
+                        is RecipeInfoState.Loading -> "Loading $recipeId"
+                        is RecipeInfoState.Success -> (recipeInfoState.value as RecipeInfoState.Success).data.toString()
+                        is RecipeInfoState.Error -> (recipeInfoState.value as RecipeInfoState.Error).error.toString()
+                    }
+                )
+            }
         }
-    )
+    }
+
+
 }
---
+++ app/src/main/java/com/disgust/sereda/utils/commonViews/ImageView.kt
@@ -0,0 +1,26 @@
+package com.disgust.sereda.utils.commonViews
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import coil.compose.AsyncImage
+import com.disgust.sereda.R
+
+@Composable
+fun ImageIngredientView(url: String) {
+    ImageView(url = "https://spoonacular.com/cdn/ingredients_500x500/$url")
+}
+
+@Composable
+fun ImageRecipeView(url: String) {
+    ImageView(url = url)
+}
+
+@Composable
+private fun ImageView(url: String) {
+    AsyncImage(
+        model = url,
+        contentDescription = null,
+        placeholder = painterResource(id = R.drawable.ic_load_test_24),
+        error = painterResource(id = R.drawable.ic_error_test_24)
+    )
+}
