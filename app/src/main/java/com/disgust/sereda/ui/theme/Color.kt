4df9a086f785cb3ac4f7ac754de1e7cbd66d86d0
Alexandr <sanchello2001@yandex.ru>
2023-02-02 18:33:54 +0300

    settings Jetpack Compose's theming APIs to style application

--- app/src/main/java/com/disgust/sereda/ui/theme/Color.kt
+++ app/src/main/java/com/disgust/sereda/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.disgust.sereda.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Green300 = Color(0xffb8ff88)
+val Green400 = Color(0xff78E86F)
+val Gray100 = Color(0xfff2f2f2)
+val Gray800 = Color(0xff515151)
